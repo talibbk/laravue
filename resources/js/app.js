ce4e231c57d37cb80f2077b8d54ade9f8151dec5
user <user@user-no-MacBook-Pro.local>
2019-04-22 14:02:12 +0900

    Update(route,dashboard deign)

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,18 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+})
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +41,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
