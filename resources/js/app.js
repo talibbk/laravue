5621f9c56564044f8a5a47baa87a3cc41c9f1d00
user <user@user-no-MacBook-Pro.local>
2019-05-13 12:10:49 +0900

    add, display users

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,12 +8,20 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import { Form,HasError,AlertError} from 'vform';
+
+window.form = Form;
+Vue.component(HasError.name,HasError)
+Vue.component(AlertError.name,AlertError)
+
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
-    { path: '/profile', component: require('./components/Profile.vue').default }
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default }
   ]
 
 const router = new VueRouter({
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add New <i class="fas fa-user-plus fa-fw"> </i></button>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Registered At</th>
+                    <th>Modify</th>
+                  </tr>
+                  <tr v-for="user in users" :key="user.id">
+                    <td>{{user.id}}</td>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.type}}</td>
+                    <td>{{user.created_at}}</td>
+                    <td>
+                        <a href="#">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+                        
+                    </td>
+                  </tr>
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
+                  <!-- Modal -->
+            <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+            <form @submit.prevent="createUser">
+
+                <div class="modal-body">
+                    <div class="form-group">                        
+                        <input v-model="form.name" type="text" name="name" placeholder="Name"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+
+                    <div class="form-group">                        
+                        <input v-model="form.email" type="email" name="email" 
+                        placeholder="Email Address"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+
+                    <div class="form-group">                        
+                        <textarea v-model="form.bio" name="bio" id="bio"
+                        placeholder="Short bio for user (Optional)"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                        <has-error :form="form" field="bio"></has-error>
+                    </div>
+
+                    <div class="form-group">                        
+                        <select v-model="form.type" id="type" name="type" 
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                        
+                            <option value="" selected>Select User Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">Standard User</option>
+                            <option value="author">Author</option>
+                        </select>
+                        <has-error :form="form" field="type"></has-error>
+                    </div>
+
+                    <div class="form-group">                        
+                        <input v-model="form.password" type="password" name="password" id="password" 
+                        placeholder="Password"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+
+            </form>
+                </div>
+            </div>
+            </div>
+
+        </div>
+</template>
+
+<script>
+import { Form, HasError, AlertError } from 'vform'
+
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+    export default {
+        data() {
+            return{
+                users:{},
+                form: new Form({
+                    name :  '',
+                    email : '',
+                    password : '',
+                    type : '',
+                    bio : '',
+                    photo : ''
+                })
+            }
+        },
+        methods:{
+            loadUsers(){
+                axios.get("api/user").then(({data})=>(this.users = data.data));
+            },
+            createUser(){
+               this.form.post('api/user'); 
+            }
+        },
+        created() {
+            this.loadUsers();
+        }
+    }
+</script>
